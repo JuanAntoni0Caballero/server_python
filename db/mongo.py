0bd70fd2e97646d8cc861553bac0035886624039
Juan Antonio Caballero <j.antoniocaballero@hotmail.es>
2025-05-05 11:38:17 +0200

    Created auth endponts

--- db/mongo.py
+++ db/mongo.py
@@ -1,7 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.database import Database
+from config import MONGO_URL
 
-MONGO_URL = "mongodb+srv://Admin:Admin1234@gamescorehub.osv9i1y.mongodb.net/" 
 
 client = AsyncIOMotorClient(MONGO_URL)
 db: Database = client["game_score_hub"]
